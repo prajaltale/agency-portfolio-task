4e7edc91e82840b16bc17f1625b60288a7ef5694
astha panchal <asthapanchal@gmail.com>
2025-12-09 23:32:02 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,102 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for, flash
+from werkzeug.utils import secure_filename
+from models import db, Project, Client, Contact, Subscriber
+from PIL import Image
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dev_secret_key' # Change this for production
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+
+# Initialize DB
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+def save_and_crop_image(form_picture, target_size):
+    random_hex = os.urandom(8).hex()
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/uploads', picture_fn)
+
+    i = Image.open(form_picture)
+    i.thumbnail(target_size)
+    new_img = Image.new("RGB", target_size, (255, 255, 255))
+    new_img.paste(i, ((target_size[0]-i.size[0])//2, (target_size[1]-i.size[1])//2))
+    new_img.save(picture_path)
+    return picture_fn
+
+@app.route('/')
+def index():
+    projects = Project.query.all()
+    clients = Client.query.all()
+    return render_template('index.html', projects=projects, clients=clients)
+
+# FIXED: Contact Form Route
+@app.route('/contact', methods=['POST'])
+def contact():
+    # Only process if it's a POST request
+    if request.method == 'POST':
+        full_name = request.form.get('full_name')
+        email = request.form.get('email')
+        mobile = request.form.get('mobile')
+        city = request.form.get('city')
+
+        # Simple validation
+        if full_name and email:
+            new_contact = Contact(full_name=full_name, email=email, mobile=mobile, city=city)
+            db.session.add(new_contact)
+            db.session.commit()
+            flash('Your quote request has been sent!', 'success')
+        else:
+            flash('Please fill in all fields.', 'danger')
+            
+    return redirect(url_for('index'))
+
+# FIXED: Subscribe Route
+@app.route('/subscribe', methods=['POST'])
+def subscribe():
+    email = request.form.get('email')
+    if email:
+        existing = Subscriber.query.filter_by(email=email).first()
+        if not existing:
+            new_sub = Subscriber(email=email)
+            db.session.add(new_sub)
+            db.session.commit()
+            flash('Successfully subscribed!', 'success')
+        else:
+            flash('You are already subscribed.', 'info')
+    return redirect(url_for('index'))
+
+@app.route('/admin')
+def admin_dashboard():
+    contacts = Contact.query.all()
+    subscribers = Subscriber.query.all()
+    return render_template('admin_dashboard.html', contacts=contacts, subscribers=subscribers)
+
+@app.route('/admin/add_project', methods=['GET', 'POST'])
+def add_project():
+    if request.method == 'POST':
+        pic = request.files['image']
+        filename = save_and_crop_image(pic, (450, 350))
+        project = Project(name=request.form['name'], description=request.form['description'], image_file=filename)
+        db.session.add(project)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('admin_project.html')
+
+@app.route('/admin/add_client', methods=['GET', 'POST'])
+def add_client():
+    if request.method == 'POST':
+        pic = request.files['image']
+        filename = save_and_crop_image(pic, (300, 300))
+        client = Client(name=request.form['name'], designation=request.form['designation'], description=request.form['description'], image_file=filename)
+        db.session.add(client)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('admin_client.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    image_file = db.Column(db.String(100), nullable=False)
+
+class Client(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    designation = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    image_file = db.Column(db.String(100), nullable=False)
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    mobile = db.Column(db.String(20), nullable=False)
+    city = db.Column(db.String(100), nullable=False)
+
+class Subscriber(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
